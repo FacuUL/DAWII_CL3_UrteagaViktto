88db245030940001143ea238e967cfce3b14ee01
FacuUL <facundodioses05@gmail.com>
2023-11-25 19:41:35 -0500

    pregunta3

--- src/app/preguntas/pregunta3/pregunta3.component.ts
+++ src/app/preguntas/pregunta3/pregunta3.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrl: './pregunta3.component.css'
 })
 export class Pregunta3Component {
+  montoPrestamo: number = 0;
+  cuotas: number = 0;
+  montoCuota: number = 0;
+  interes: number = 0.10;
 
+  calcularCuotas() {
+    if (this.montoPrestamo >= 5000) {
+      this.cuotas = 3;
+    } else if (this.montoPrestamo <= 1000) {
+      this.cuotas = 1;
+    } else if (this.montoPrestamo >= 2000 && this.montoPrestamo <= 3000) {
+      this.cuotas = 2;
+    } else {
+      this.cuotas = 5;
+    }
+
+ 
+    this.interes = this.montoPrestamo < 4000 ? 0.12 : 0.10;
+    const montoTotal = this.montoPrestamo * (1 + this.interes);
+    this.montoCuota = montoTotal / this.cuotas;
+  }
 }
